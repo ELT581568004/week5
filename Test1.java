d9bc083caed01023adca92011149712ed7ef5bc1
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-10-07 09:03:11 +0700

    Init Starter Code

---
+++ Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+  public static void main(String[] args) {
+    nPrintln(5, "Welcome to Java!");
+  }
+  public static void nPrintln(String message, int n) {
+    int n = 1;
+    for (int i = 0; i < n; i++)
+      System.out.println(message);
+  }
+}
