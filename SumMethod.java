c8b6bfc2aea44ee8ea251c32c18c6c06a8b7c6a5
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-10-07 08:32:26 +0700

    Init Starter Code

---
+++ SumMethod.java
@@ -0,0 +1,13 @@
+public class SumMethod {
+  public static void main(String[] args) {
+
+  }
+
+  public static int sum(int i1, int i2) {
+    int result = 0;
+
+    // Your code
+
+    return result;
+  }
+}
